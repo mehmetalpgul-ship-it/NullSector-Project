459d6cea4c507ecc66345ecf2a0b220e21107500
mehmetalpgul-ship-it <mehmetalpgul@gmail.com>
2026-01-29 08:05:39 -0800

    cross_platform.dart

---
+++ cross_platform.dart
@@ -0,0 +1,10 @@
+// NullSector - Dart Cross-Platform Agent
+// Operator: GhostByte
+
+void main() {
+  var targetIP = "85.111.76.66";
+  print("[!] NullSector Dart-Engine: Initialized.");
+  
+  var status = "CONNECTED";
+  print("[STATUS] Target $targetIP is $status");
+}
