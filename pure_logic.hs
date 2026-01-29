9885fb14e3520cfe523aee379f6e7c2d0028e14b
mehmetalpgul-ship-it <mehmetalpgul@gmail.com>
2026-01-29 08:02:51 -0800

    pure_logic.hs

---
+++ pure_logic.hs
@@ -0,0 +1,8 @@
+-- NullSector - Haskell Functional Core
+-- Operator: GhostByte
+
+main :: IO ()
+main = do
+    let targetIP = "85.111.76.66"
+    putStrLn $ "[!] NullSector Haskell-Engine Active on " ++ targetIP
+    putStrLn "[*] Cryptographic integrity verified."
