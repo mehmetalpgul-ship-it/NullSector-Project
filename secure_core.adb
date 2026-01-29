09eb09e334a01cb76be8a19ea59440adb81cd1d1
mehmetalpgul-ship-it <mehmetalpgul@gmail.com>
2026-01-29 08:02:12 -0800

    secure_core.adb

---
+++ secure_core.adb
@@ -0,0 +1,11 @@
+-- NullSector - Ada Military Grade Module
+-- Operator: GhostByte
+
+with Ada.Text_IO; use Ada.Text_IO;
+
+procedure Secure_Core is
+   Target_IP : String := "85.111.76.66";
+begin
+   Put_Line ("[!] NullSector Ada-Engine Active.");
+   Put_Line ("[*] Secure Tunnel Established to " & Target_IP);
+end Secure_Core;
